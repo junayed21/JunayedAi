66d8150af12f819b6f71bdfe7048f0bda73f715e
Shaidul10 <asmaaktersha10@gmail.com>
2024-12-12 13:01:15 +0600

    Update 00info.js

--- scripts/cmds/00info.js
+++ scripts/cmds/00info.js
@@ -15,17 +15,17 @@ module.exports = {
 		envConfig: {}
 	},
 	onStart: async function ({ message }) {
-		const authorName = " ⩸ᗷᗩᎩᒍᏆᗞ⩸ ";
-		const ownAge = "『 ⩸__17__⩸ 』";
-		const messenger = "https://m.me/100065506668822";
-		const authorFB = "https://www.facebook.com/profile.php?id=BAYJID.500k";
-		const authorNumber = "_01638007072";
-		const Status = "⩸__🆂🅸🅽🅶🅻🅴__⩸";
+		const authorName = " JUNAYED ";
+		const ownAge = "『 19+ 』";
+		const messenger = "https://m.me/100076343334427";
+		const authorFB = "https://www.facebook.com/profile.php?id=Mdyeasin.Arafat.Junayed";
+		const authorNumber = "_01845546501";
+		const Status = "Married";
 		const urls = [
-"https://i.imgur.com/I8JSUWI.jpeg",
-"https://i.imgur.com/I8JSUWI.jpeg",
-"https://i.imgur.com/I8JSUWI.jpeg",
-"https://i.imgur.com/I8JSUWI.jpeg"
+"",
+"",
+"",
+""
 ];
 		const link = urls[Math.floor(Math.random() * urls.length)];
 		const now = moment().tz('Asia/Jakarta');
@@ -51,11 +51,11 @@ module.exports = {
 \⏰彡𝑁𝑜𝑤 𝑇𝑖𝑚𝑒 : ${time}
 \🔰彡𝐴𝑛𝑦 𝐻𝑒𝑙𝑝 𝐶𝑜𝑛𝑡𝑎𝑐𝑡 :⩸__${messenger}__⩸
 \📛彡𝐵𝑜𝑡 𝐼𝑠 𝑅𝑢𝑛𝑛𝑖𝑛𝑔 𝐹𝑜𝑟 : ${uptimeString}
-    𝑻𝒈: https://t.me/MOHAMMADBAYJID
-    𝑰𝒏𝒔𝒕𝒂: https://www.instagram.com/mr_bayjid120?
-    𝑪𝒂𝒑𝑪𝒖𝒕: copy_bayjid
-    𝑻𝒊𝒌𝑻𝒐𝒌: https://www.tiktok.com/@copy_bayjid?
-    𝒀𝒐𝒖𝑻𝒖𝒃𝒆: https://youtube.com/@Bayjid Editz?
+    𝑻𝒈: https://t.me/junayed69
+    𝑰𝒏𝒔𝒕𝒂: https://www.instagram.com/jun_a_yed69?
+    𝑪𝒂𝒑𝑪𝒖𝒕: nai
+    𝑻𝒊𝒌𝑻𝒐𝒌: https://www.tiktok.com/@junayed69xyz?
+    𝒀𝒐𝒖𝑻𝒖𝒃𝒆: https://youtube.com/@nai?
 \===============`,
 			attachment: await global.utils.getStreamFromURL(link)
 		});

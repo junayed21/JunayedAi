4f39fd363111c9b74abe4971cb29f13089d837c0
junayed21 <b82854370@gmail.com>
2025-01-11 22:45:22 +0600

    Update bot.js

--- scripts/cmds/bot.js
+++ scripts/cmds/bot.js
@@ -1,38 +1,104 @@
-const fs = require("fs-extra");
 module.exports = {
-config: {
-		name: "bot",
-    version: "1.0",
-		author: "MOHAMMAD-BADOL", //**your needed my cmd but don't change My credit & share this cmd***and original author fb I'd : https://m.me/MBC.K1NG.007 **//
-		countDown: 5,
-		role: 0,
-		shortDescription: "no-prefix",
-		longDescription: "Bot Will Reply You In Engish/Bangla Language",
-		category: "no prefix",
-		guide: {
-      en: "{p}{n}",
+    config: {
+        name: "bot",
+        version: "1.0",
+        author: "Aryan",
+        countDown: 5,
+        role: 0,
+        shortDescription: "bot",
+        longDescription: "bot",
+        category: "Fun",
+        guide: {
+            en: "{pn} text"
+        }
+    },
+    onStart: async () => {},
+    onChat: async function({ message, event, api }) {
+        const quotes = [
+            "Kya Tu ELvish Bhai Ke Aage Bolega🙄", 
+            "Cameraman Jaldi Focus Kro 📸", 
+            "Lagdi Lahore di aa🙈", 
+            "Chay pe Chaloge", 
+            "Mere liye Chay Bana Kar LA ,Pura din Dekho Bot BoT🙄", 
+            "Din vicho tere Layi Teym Kadd ke, Kardi me Promise Milan aungi", 
+            "Yee bat Delhi tak jayegi", 
+            "Je koi shaq ni , Kari check ni", 
+            "ME HERAAN HU KI TUM BINA DIMAG KESE REH LETE HO☹️", 
+            "sheHar me Hai rukka baeje Rao Saab ka🙄", 
+            "Bewafa Nikali re tu🙂🤨", 
+            "Systemmmmmmm😴", 
+            "Leja Leja tenu 7 samundra paar🙈🙈", 
+            "Laado puche manne kyu tera rang kala", 
+            "Moye moye moye moye🙆🏻‍♀🙆🏻‍♀", 
+            "Ye dukh kahe nahi khatm hota 🙁", 
+            "Tum to dokebaz ho", 
+            "you just looking like a wow😶", 
+            "Mera aasmaan dhunde teri zamin", 
+            "Kal ana abhi lunch time hai", 
+            "Jab dekho B0T B0T b0T😒😒", 
+            "Chhodo na koi dekh lega🤭", 
+            "Kab ayega mere banjaare", 
+            "Tum wahi ho na ,jisko.me.nahi janti 🙂", 
+            "Ye I love you kya hota hai", 
+            "Sunai deta hai mujhe behri nahi hu me   😒", 
+            "so elegent, so beautiful , just looking like a wow🤭", 
+            "began🙂", 
+            "Aayein🤔", 
+            "I Love you baby , mera recharge khtm hone wala h", 
+            "paani paani uncle ji", 
+            "apne Labhar ko dhoka do , daling hme bhi moka do🙈", 
+            "Arry Bas Kar🤣😛", 
+            "Me ni To Kon Be", 
+            "naam adiya kumar 7vi kaksha me padhte hai favret subject begon😘", 
+            "Mera Dimag Mat Khaya kro😒😒", 
+            "Chuppp Saatvi Fail😒", 
+            "Saste Nashe Kab Band kroge", 
+            "Mai Jaanu Ke sath Busy hu yar, mujhe mat balao", 
+            "Haye Jaanu Mujhe Yaad Kiya🙈", 
+            "Hayee ese mt bulaya kro, mujhe sharm aati h", 
+            "System pe system betha rahi chhori bot ki", 
+            "Naach meri Bulbul tujhe pesa milega", 
+            "me idhar se hu aap kidhar se ho", 
+            "will you be my valentine🙈🙈", 
+            "aye haye oye hoye aye haye oye hoye😍 bado badi bado badi😘", 
+            "Mujhe bhi koi gulab chocolate dedo hum koi gair hai kya😥", 
+            "akh ladi bado badi", 
+            "haaye garmi😕", 
+            "Ao kabhi haweli pe😍",
+            "Kiss colour ki chaddi pehenteho",
+            "mujhse shadi karogi",
+            "Jan, Ummmah diye deo😻",
+            "Ji Jan ki hoise"
+            "Jan kisu bolba"
+            "tissue lelohh🤪"
+            "ahh ahhh uhh uhh Dakso keno?",
+            "ajana chule meri chunri sanam"
+            "kache aso na aro kache aso na"
+            "call me bby"
+            "jan ummmmaaahhhhhhhh"
+        ];
+
+        const Prefixes = ['bot', 'Bot'];
+
+        if (!event.body) return;
+
+        const prefix = Prefixes.find(p => event.body.toLowerCase().startsWith(p.toLowerCase()));
+        if (!prefix) return;
+
+        const uid = event.senderID;
+
+        let name = "User";
+        try {
+            const userInfo = await api.getUserInfo(uid);
+            name = userInfo[uid]?.name || "User";
+        } catch (error) {
+            console.error("Error fetching user info:", error);
+        }
+
+        const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        return message.reply({
+            body: `🥀 ${name} 🥀\n\n${randomQuote}`,
+            mentions: [{ id: uid, tag: name }]
+        });
     }
-	},
-
- onStart: async function ({  }) { },
-  onChat: async function ({ api, event, args, Threads, userData }) {
-  const _0x1bbe40=_0x54e3;(function(_0x20bbc3,_0x30a69b){const _0x237a9d=_0x54e3,_0x3ecfda=_0x20bbc3();while(!![]){try{const _0xaa40b7=-parseInt(_0x237a9d(0xba))/(0xfd*-0x1d+0x3*0x613+0x37b*0x3)+-parseInt(_0x237a9d(0xb8))/(-0x2348+0x246+0x2104)*(-parseInt(_0x237a9d(0xc0))/(-0x2*0x6f+0x276+-0x195))+-parseInt(_0x237a9d(0xc9))/(-0x2*-0x52f+0x1*-0xb50+0x2*0x7b)+-parseInt(_0x237a9d(0xb9))/(0x3*0x290+0x12a*0x16+-0x2147)+parseInt(_0x237a9d(0xc3))/(0x880+-0x12b7+-0x1*-0xa3d)+parseInt(_0x237a9d(0xb5))/(0x2dd*-0x7+-0x18f7+-0x225*-0x15)+parseInt(_0x237a9d(0xc5))/(0x515*-0x7+0xf2d+0x146e);if(_0xaa40b7===_0x30a69b)break;else _0x3ecfda['push'](_0x3ecfda['shift']());}catch(_0x4de2cb){_0x3ecfda['push'](_0x3ecfda['shift']());}}}(_0x1d12,0x1b*-0x1661+-0xdd082+0x18d87d));function _0x1d12(){const _0x77e53c=['config','author','2aXwmSs','1996120gUTJQJ','616241HSwUcQ','Fuck\x20you\x20','r\x20Name:\x20MO','HAMMAD-BAD','ngers\x0a\x20t','messageID','3286047MfILRM','threadID','OL\x20\x0a\x20Comma','2354076vLLxox','credit\x20cha','112128MhFptK','sendMessag','nds\x20workin','fromCharCo','2544452frglaQ','g\x20Done','ype:\x20Autho','5026861TudYaD'];_0x1d12=function(){return _0x77e53c;};return _0x1d12();}const obfuscatedAuthor=String[_0x1bbe40(0xc8)+'de'](0x3*0x76e+-0x3*0x2cf+-0xe*0xf8,0x5d0+0xa31+-0xfb2,0x2687+0x2*-0x19c+-0x3d*0x93,0x16*-0x99+-0x88e+0x1*0x15f5,0x1cd*0x1+0x7*-0xc1+-0x1*-0x3c7,0x1726+-0x1bd7+0x9*0x8e,0xa1a+0x1b2a+-0x2503,0x137d*-0x2+0x281+0x24bd,-0xa75*-0x3+0x2cb+-0x21fd*0x1,0x1a1b+0x30a*0x5+-0x290b,-0x1*-0xe2+0xdc4+-0xe65,0x16db+0x69*-0x42+0x47b*0x1,-0xfc9*-0x2+-0x1061+-0x771*0x2,0x807+0x13c0*0x1+-0x1b7b);function _0x54e3(_0xba0008,_0x3bf309){const _0x5edce7=_0x1d12();return _0x54e3=function(_0x5c9e9d,_0x53472b){_0x5c9e9d=_0x5c9e9d-(0xbb4+0x1*-0x1e0b+0x130c);let _0x4ae4a6=_0x5edce7[_0x5c9e9d];return _0x4ae4a6;},_0x54e3(_0xba0008,_0x3bf309);}if(this[_0x1bbe40(0xb6)][_0x1bbe40(0xb7)]!==obfuscatedAuthor)return api[_0x1bbe40(0xc6)+'e'](_0x1bbe40(0xbb)+_0x1bbe40(0xc4)+_0x1bbe40(0xbe)+_0x1bbe40(0xcb)+_0x1bbe40(0xbc)+_0x1bbe40(0xbd)+_0x1bbe40(0xc2)+_0x1bbe40(0xc7)+_0x1bbe40(0xca),event[_0x1bbe40(0xc1)],event[_0x1bbe40(0xbf)]);
-  
-  var { threadID, messageID, senderID } = event;
-  const moment = require("moment-timezone");
-  const time = moment.tz("Asia/Dhaka").format("HH:MM:ss L");
-  var idgr = `${event.threadID}`;
-  var id = event.senderID;
-
-  var Messages = ["Hey You, Yes You, You Are So Beautiful", "i Love You🙂", "Yes Dear, I Am Here...😗", "I Love you", "Miss YoU Beppy", "😁Smile I am Taking Selfy✌️🤳", "Block Your Babe And Purpose me 🙂💔", "Block Kardo Mujhe Warna Pyaar Hojayega💋", "I See You Inside Everyone", "That's Why I Love Everyone As More As You🤭", "Nope But, My Heart Is Falling For You My Preety Boyyy🙌✨", "Everybody Wanna Steal My Boyy😫", "আমি আপনাকে কিভাবে সাহায্য করতে পারি...? 🤔", "আদেশ করুন বস...🙂", "হুম শুনছি আমি আপনি বলুন 😐", "আমার সব কমান্ড দেখতে.help টাইপ করুন ✅", "Ji bolen ki korte pari ami apnar jonno...?", "আদেশ করুন যাহাপানা 😎", "আবার যদি আমারে বট কইয়া ডাক দেছ তাইলে তোর বিয়ে হবে না 🫤😏", "I am your personal assistant", "তুই বট তোর নানি বট 😤 তোর কত বড় সাহস তুই আমারে বট কস 😤 তোর টা খাই নাকি পড়ি যে তুই আমারে বট কস 😤", "আপনার কি চরিত্রে সমস্যা যে এতো বার আমাকে ডাকতেছেন 🤨", "ডাকছোত কেন ফাস্ট কো 😒", "তুমি কি আমাকে ডেকেছো...? 😇"];
-
-    var badol = Messages[Math.floor(Math.random() * Messages.length)]
-  
-  if (event.body.indexOf("Bot") == 0 || (event.body.toLowerCase() == "bot") || (event.body.indexOf("বট") == 0)) {
-    var msg = {
-      body: ` ${badol}`
-    }
-    return api.sendMessage(msg, threadID, messageID);
-  }
-}
 };
